215a3ab3a0735cd7e317074c3fe0e594736adbd7
JaladeAlan <74614392+JaladeAlan@users.noreply.github.com>
2023-09-06 20:51:10 +0100

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,6 +1,6 @@
 <?php
 require "database.php";
-$db = new DataBase();
+$db = new Database();
 if (isset($_POST['fullname']) && isset($_POST['email']) && isset($_POST['username']) && isset($_POST['password'])) {
     if ($db->dbConnect()) {
         if ($db->signUp("users", $_POST['fullname'], $_POST['email'], $_POST['username'], $_POST['password'])) {
